f376064613e7148a1209fde10290396193cb95cc
sneurlax <sneurlax@gmail.com>
2017-07-08 15:28:36 -0500

    move test.js into monitor.js for hygiene purposes

---
+++ monitor.js
@@ -0,0 +1,86 @@
+var rpcAuth = require('./rpcAuth.js');
+var bitcoin = require('./node_modules/bitcoin')
+
+var rpcClient = new bitcoin.Client({
+  host: 'localhost',
+  port: 9266,
+  user: rpcAuth.rpcuser,
+  pass: rpcAuth.rpcpassword,
+  timeout: 30000
+});
+
+var low = require('lowdb');
+var db = low('bitmark.json', { storage: require('lowdb/lib/storages/file-async') });
+db.defaults({ block: {} })
+  .write();
+
+// db
+//   .set('block', {height: 'height2', hash: 'hash2', info: {}})
+//   .write();
+
+var startHeight = 286441; // Auditing began on 2017/07/07
+var maxHeight = 287550; // Some 'high' block height that we know exists
+var delay = 0;
+
+var blockHeight = startHeight;
+var monitor = function() {
+  if( blockHeight <= maxHeight ) {
+    rpcClient.getBlockHash(blockHeight, function(err, blockHash, resHeaders) {
+      if (err) return console.log(err);
+
+      rpcClient.getBlock(blockHash, function(err, blockInfo, resHeaders) {
+        if (err) return console.log(err);
+
+        blockObj = {}
+        blockObj[blockInfo['hash']] = blockInfo;
+
+        var heightFound = db.has(blockInfo['height']).value()
+        var hashFound = db.has(blockInfo['height']+'.'+blockInfo['hash']).value()
+
+        if( !heightFound && !hashFound ) {
+          db
+            .set(blockInfo['height'], blockObj)
+            .write();
+          console.log('Recorded block '+blockInfo['hash']+' at height '+blockInfo['height']);
+        } else if( heightFound && !hashFound ) {
+          console.log('Orphan detected at height '+blockInfo['height']);
+
+          var newBlockInfo = db.get(blockInfo['height']).value();
+          // oldBlockInfo[blockInfo['hash']] = blockObj;
+          newBlockInfo[blockInfo['hash']+'asdf'] = blockInfo;
+
+          db
+            .set(blockInfo['height'], newBlockInfo, blockObj)
+            .write();
+          console.log('Recorded new block '+blockInfo['hash']+' at height '+blockInfo['height']);
+        } else {
+          console.log('Found block '+blockInfo['hash']+' at height '+blockInfo['height']);
+        }
+      });
+    });
+
+    blockHeight++;
+
+    // if( blockHeight > 287500 )
+    //   break;
+  } else {
+    if( maxHeight != 287550 ) {
+      delay = 3000;
+    } else {
+      if( delay == 0 ) {
+        currentHeight = rpcClient.getBlockCount( function(err, result, resHeaders) {
+          if (err) return console.log(err);
+          maxHeight = result;
+          delay = 0;
+          return
+        });
+        delay = 100;
+      }
+    }
+    console.log('Waiting for new block.  Current height '+(blockHeight-1))
+    // break;
+  }
+  setTimeout(monitor, delay);
+}
+
+monitor();

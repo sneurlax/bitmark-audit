b79ae70ac8e5a6fd5135b1174a33c9d4ace251bc
sneurlax <sneurlax@gmail.com>
2017-07-10 17:35:48 -0500

    verify block hashes match previousblockhash, etc.

--- test.js
+++ test.js
@@ -22,6 +22,14 @@ for( height in blocks ) {
   if( Object.keys(blocks[height]).length > 1 ) {
     console.log('Height '+height+': FAILED CHECK ('+Object.keys(blocks[height]).length+' blocks found)');
   } else {
-    console.log('Height '+height+': OK');
+    if( db.has('block.'+(height-1)).value() ) { // only false for first recorded block
+      if( blocks[height][Object.keys(blocks[height])[Object.keys(blocks[height]).length-1]]['previousblockhash'] == Object.keys(db.get('block.'+(height-1)).value())[Object.keys(blocks[height-1]).length-1] ) { // current block's previousblockhash equals the previous block's hash
+        console.log('Height '+height+': OK');
+      } else {
+        console.log('Height '+height+': FAILED CHECK (expected previousblockhash '+(blocks[height][Object.keys(blocks[height])[0]]['previousblockhash']).substr(0,6)+'..., found '(Object.keys(db.get('block.'+(height-1)).value())[0]).substr(0,6)+'...)');
+      }
+    } else { // first recorded block; don't/can't check previous block hash
+      console.log('Height '+height+': OK');
+    }
   }
 }

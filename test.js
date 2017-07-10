422fcb7b5fb988abdc2fd6496da654b55a834ed9
sneurlax <sneurlax@gmail.com>
2017-07-10 17:36:03 -0500

    only log heights that fail a check

--- test.js
+++ test.js
@@ -24,12 +24,12 @@ for( height in blocks ) {
   } else {
     if( db.has('block.'+(height-1)).value() ) { // only false for first recorded block
       if( blocks[height][Object.keys(blocks[height])[Object.keys(blocks[height]).length-1]]['previousblockhash'] == Object.keys(db.get('block.'+(height-1)).value())[Object.keys(blocks[height-1]).length-1] ) { // current block's previousblockhash equals the previous block's hash
-        console.log('Height '+height+': OK');
+        // console.log('Height '+height+': OK');
       } else {
         console.log('Height '+height+': FAILED CHECK (expected previousblockhash '+(blocks[height][Object.keys(blocks[height])[0]]['previousblockhash']).substr(0,6)+'..., found '(Object.keys(db.get('block.'+(height-1)).value())[0]).substr(0,6)+'...)');
       }
     } else { // first recorded block; don't/can't check previous block hash
-      console.log('Height '+height+': OK');
+      // console.log('Height '+height+': OK');
     }
   }
 }

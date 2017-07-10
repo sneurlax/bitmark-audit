6c3086e57ff0bd3dc59ccbae3df47059be62d4df
sneurlax <sneurlax@gmail.com>
2017-07-10 17:35:32 -0500

    check each recorded block height for duplicates

--- test.js
+++ test.js
@@ -14,8 +14,14 @@ var db = low('bitmark.json', { storage: require('lowdb/lib/storages/file-async')
 db.defaults({ block: {} })
   .write();
 
+var anomolousHeight = []
 var blocks = db.get('block').value();
 
 for( height in blocks ) {
-  console.log('At height '+height+', '+Object.keys(blocks[height]).length+' blocks exist.');
+
+  if( Object.keys(blocks[height]).length > 1 ) {
+    console.log('Height '+height+': FAILED CHECK ('+Object.keys(blocks[height]).length+' blocks found)');
+  } else {
+    console.log('Height '+height+': OK');
+  }
 }

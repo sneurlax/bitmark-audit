b11f2f06238afa6df9642a5c0e3fafbe4b9deb28
sneurlax <sneurlax@gmail.com>
2017-07-10 17:36:23 -0500

    remove unused list

--- test.js
+++ test.js
@@ -14,7 +14,6 @@ var db = low('bitmark.json', { storage: require('lowdb/lib/storages/file-async')
 db.defaults({ block: {} })
   .write();
 
-var anomolousHeight = []
 var blocks = db.get('block').value();
 
 for( height in blocks ) {
